7851ff7e4443992e7e6178731c26ca6aa00071d6
Shcoobz <a.boeckmann23@gmail.com>
2023-10-13 11:46:01 +0200

    added constants for frequently used strings

--- src/main/java/com/codecool/mightytextadventure/data/AreaConnections.java
+++ src/main/java/com/codecool/mightytextadventure/data/AreaConnections.java
@@ -4,6 +4,12 @@ import java.util.Map;
 
 public class AreaConnections {
 
+  public static final String LEAVE = "[Leave]";
+  public static final String NORTH = "[Go North]";
+  public static final String EAST = "[Go East]";
+  public static final String SOUTH = "[Go South]";
+  public static final String WEST = "[Go West]";
+
   // private constructor to prevent instantiation
   private AreaConnections() {
     throw new AssertionError("Cannot instantiate WayToForrest class");
@@ -12,24 +18,24 @@ public class AreaConnections {
   public static void setupStart(Map<AreaName, Area> areas) {
     // ========== STARTING AREA ==========
     areas.get(AreaName.STARTING_AREA).addConnection("[Knock]", areas.get(AreaName.SA_KNOCK));
-    areas.get(AreaName.STARTING_AREA).addConnection("[Leave]", areas.get(AreaName.GAME_OVER));
+    areas.get(AreaName.STARTING_AREA).addConnection(LEAVE, areas.get(AreaName.GAME_OVER));
 
     // ========== SA_KNOCKING ==========
     areas.get(AreaName.SA_KNOCK).addConnection("[Knock again]", areas.get(AreaName.SA_KNOCK_AGAIN));
     areas.get(AreaName.SA_KNOCK).addConnection("[Call his name]", areas.get(AreaName.SA_CALL_NAME));
-    areas.get(AreaName.SA_KNOCK).addConnection("[Leave]", areas.get(AreaName.GAME_OVER));
+    areas.get(AreaName.SA_KNOCK).addConnection(LEAVE, areas.get(AreaName.GAME_OVER));
 
     // ========== SA_KNOCK_AGAIN ==========
     areas.get(AreaName.SA_KNOCK_AGAIN).addConnection("[Knock yet again]", areas.get(AreaName.SA_KNOCK));
-    areas.get(AreaName.SA_KNOCK_AGAIN).addConnection("[Leave]", areas.get(AreaName.GAME_OVER));
+    areas.get(AreaName.SA_KNOCK_AGAIN).addConnection(LEAVE, areas.get(AreaName.GAME_OVER));
 
     // ========== SA_CALL_NAME ==========
     areas.get(AreaName.SA_CALL_NAME).addConnection("[Knock yet again]", areas.get(AreaName.SA_KNOCK));
     areas.get(AreaName.SA_CALL_NAME).addConnection("[Reassure him]", areas.get(AreaName.HUT_INSIDE));
-    areas.get(AreaName.SA_CALL_NAME).addConnection("[Leave]", areas.get(AreaName.GAME_OVER));
+    areas.get(AreaName.SA_CALL_NAME).addConnection(LEAVE, areas.get(AreaName.GAME_OVER));
   }
 
-  public static void setupHutInside(Map<AreaName, Area> areas){
+  public static void setupHutInside(Map<AreaName, Area> areas) {
     // ========== HUT_INSIDE ==========
     areas.get(AreaName.HUT_INSIDE).addConnection("[Enter]", areas.get(AreaName.HI_ENTER));
 
@@ -64,43 +70,41 @@ public class AreaConnections {
     areas.get(AreaName.HI_OPTION_GO_ALONE).addConnection("[Run away in fear]", areas.get(AreaName.GAME_OVER));
   }
 
-  public static void setupWayToForrest(Map<AreaName, Area> areas){
-    // ========== WAY_TO_FORREST ==========
+  public static void setupWayToForrest(Map<AreaName, Area> areas) {
     areas.get(AreaName.WAY_TO_FORREST).addConnection("[Enter]", areas.get(AreaName.FORREST_ENTER));
     areas.get(AreaName.WAY_TO_FORREST).addConnection("[Run away in fear]", areas.get(AreaName.GAME_OVER));
   }
 
-  public static void setupForrestEnter(Map<AreaName, Area> areas){
-    // ========== FORREST_ENTER ==========
-    areas.get(AreaName.FORREST_ENTER).addConnection("[Go North]", areas.get(AreaName.F_NORTH));
-    areas.get(AreaName.FORREST_ENTER).addConnection("[Go East]", areas.get(AreaName.F_EAST));
-    areas.get(AreaName.FORREST_ENTER).addConnection("[Go South]", areas.get(AreaName.F_SOUTH));
-    areas.get(AreaName.FORREST_ENTER).addConnection("[Go West]", areas.get(AreaName.F_WEST));
+  public static void setupForrestEnter(Map<AreaName, Area> areas) {
+    areas.get(AreaName.FORREST_ENTER).addConnection(NORTH, areas.get(AreaName.F_NORTH));
+    areas.get(AreaName.FORREST_ENTER).addConnection(EAST, areas.get(AreaName.F_EAST));
+    areas.get(AreaName.FORREST_ENTER).addConnection(SOUTH, areas.get(AreaName.F_SOUTH));
+    areas.get(AreaName.FORREST_ENTER).addConnection(WEST, areas.get(AreaName.F_WEST));
   }
 
-  public static void setupForrestDirections(Map<AreaName, Area> areas){
+  public static void setupForrestDirections(Map<AreaName, Area> areas) {
     // ========== F_NORTH ==========
-    areas.get(AreaName.F_NORTH).addConnection("[Go North]", areas.get(AreaName.F_NORTH));
-    areas.get(AreaName.F_NORTH).addConnection("[Go East]", areas.get(AreaName.F_EAST));
-    areas.get(AreaName.F_NORTH).addConnection("[Go South]", areas.get(AreaName.F_SOUTH));
-    areas.get(AreaName.F_NORTH).addConnection("[Go West]", areas.get(AreaName.F_WEST));
+    areas.get(AreaName.F_NORTH).addConnection(NORTH, areas.get(AreaName.F_NORTH));
+    areas.get(AreaName.F_NORTH).addConnection(EAST, areas.get(AreaName.F_EAST));
+    areas.get(AreaName.F_NORTH).addConnection(SOUTH, areas.get(AreaName.F_SOUTH));
+    areas.get(AreaName.F_NORTH).addConnection(WEST, areas.get(AreaName.F_WEST));
 
     // ========== F_EAST ==========
-    areas.get(AreaName.F_EAST).addConnection("[Go North]", areas.get(AreaName.F_NORTH));
-    areas.get(AreaName.F_EAST).addConnection("[Go East]", areas.get(AreaName.F_EAST));
-    areas.get(AreaName.F_EAST).addConnection("[Go South]", areas.get(AreaName.F_SOUTH));
-    areas.get(AreaName.F_EAST).addConnection("[Go West]", areas.get(AreaName.F_WEST));
+    areas.get(AreaName.F_EAST).addConnection(NORTH, areas.get(AreaName.F_NORTH));
+    areas.get(AreaName.F_EAST).addConnection(EAST, areas.get(AreaName.F_EAST));
+    areas.get(AreaName.F_EAST).addConnection(SOUTH, areas.get(AreaName.F_SOUTH));
+    areas.get(AreaName.F_EAST).addConnection(WEST, areas.get(AreaName.F_WEST));
 
     // ========== F_SOUTH ==========
-    areas.get(AreaName.F_SOUTH).addConnection("[Go North]", areas.get(AreaName.F_NORTH));
-    areas.get(AreaName.F_SOUTH).addConnection("[Go East]", areas.get(AreaName.F_EAST));
-    areas.get(AreaName.F_SOUTH).addConnection("[Go South]", areas.get(AreaName.F_SOUTH));
-    areas.get(AreaName.F_SOUTH).addConnection("[Go West]", areas.get(AreaName.F_WEST));
+    areas.get(AreaName.F_SOUTH).addConnection(NORTH, areas.get(AreaName.F_NORTH));
+    areas.get(AreaName.F_SOUTH).addConnection(EAST, areas.get(AreaName.F_EAST));
+    areas.get(AreaName.F_SOUTH).addConnection(SOUTH, areas.get(AreaName.F_SOUTH));
+    areas.get(AreaName.F_SOUTH).addConnection(WEST, areas.get(AreaName.F_WEST));
 
     // ========== F_WEST ==========
-    areas.get(AreaName.F_WEST).addConnection("[Go North]", areas.get(AreaName.F_NORTH));
-    areas.get(AreaName.F_WEST).addConnection("[Go East]", areas.get(AreaName.F_EAST));
-    areas.get(AreaName.F_WEST).addConnection("[Go South]", areas.get(AreaName.F_SOUTH));
-    areas.get(AreaName.F_WEST).addConnection("[Go West]", areas.get(AreaName.F_WEST));
+    areas.get(AreaName.F_WEST).addConnection(NORTH, areas.get(AreaName.F_NORTH));
+    areas.get(AreaName.F_WEST).addConnection(EAST, areas.get(AreaName.F_EAST));
+    areas.get(AreaName.F_WEST).addConnection(SOUTH, areas.get(AreaName.F_SOUTH));
+    areas.get(AreaName.F_WEST).addConnection(WEST, areas.get(AreaName.F_WEST));
   }
 }
