de532d64be970cf69b95239e9d30fa12fd9380f7
Shcoobz <a.boeckmann23@gmail.com>
2023-10-14 16:24:49 +0200

    refactored Area again

--- src/main/java/com/codecool/mightytextadventure/Application.java
+++ src/main/java/com/codecool/mightytextadventure/Application.java
@@ -1,24 +1,19 @@
 package com.codecool.mightytextadventure;
 
 import com.codecool.mightytextadventure.data.Area;
-import com.codecool.mightytextadventure.data.AreaDescription;
-import com.codecool.mightytextadventure.data.AreaConnections;
 import com.codecool.mightytextadventure.data.AreaName;
-import com.codecool.mightytextadventure.data.arearooms.Room;
-import com.codecool.mightytextadventure.data.arearooms.StartingArea;
 import com.codecool.mightytextadventure.logic.Game;
 import com.codecool.mightytextadventure.logic.Player;
 import com.codecool.mightytextadventure.ui.Display;
 import com.codecool.mightytextadventure.ui.Input;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.EnumMap;
 
 // TODO: implement back option
+// TODO: move message to display
 
 public class Application {
-  private Map<AreaName, Room> rooms = new HashMap<>();
 
   public static void main(String[] args) {
     Display display = new Display();
@@ -40,40 +35,12 @@ public class Application {
   private static Map<AreaName, Area> loadAreas() {
     Map<AreaName, Area> areas = new EnumMap<>(AreaName.class);
 
-    AreaDescription.loadStart(areas);
-    AreaDescription.loadHutInside(areas);
-    AreaDescription.loadWayToForrest(areas);
-    AreaDescription.loadForrestEnter(areas);
-    AreaDescription.loadForrestDirections(areas);
-    AreaDescription.loadFindFang(areas); // winning area
-    AreaDescription.loadGameOver(areas); // loosing area
+    Area.loadAllAreas(areas);
 
     return areas;
   }
 
   private static void setupAreaConnections(Map<AreaName, Area> areas) {
-    AreaConnections.setupStart(areas);
-    AreaConnections.setupHutInside(areas);
-    AreaConnections.setupWayToForrest(areas);
-    AreaConnections.setupForrestEnter(areas);
-    AreaConnections.setupForrestDirections(areas);
+    Area.setupAreaConnections(areas);
   }
-
-/*  private void initRooms() {
-    rooms.put(AreaName.STARTING_AREA, new Room("""
-        On the muddy path, Hagrid's rustic hut stands prominently, its patchwork roof contrasting the overcast sky. Rain gently
-        mists your face, carrying scents of damp earth and woodsmoke, suggesting a fire within. A dim light shines from a
-        window, causing raindrops to shimmer. To your left, the Forbidden Forest displays muted autumn hues, punctuated by
-        occasional owl hoots and rustlings of unseen creatures.
-        """));
-
-    rooms.put(AreaName.SA_KNOCK, new Room("""
-        You knock on Hagrid's door, the sound contrasting the steady rain. Silence ensues; no footsteps, no reply.
-        """));
-  }
-
-  private void initConnections() {
-  rooms.get(AreaName.STARTING_AREA).addConnection("[Knock]",rooms.get(AreaName.SA_KNOCK));
-  }*/
-
 }
